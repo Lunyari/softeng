355f05ecdb83376f720fc6202894ddb6cb7e63e4
clarence <arenzmauricio12@gmail.com>
2023-07-03 01:58:27 +0800

    lol

--- leagueDisplay.js
+++ leagueDisplay.js
@@ -14,10 +14,32 @@ const firebaseConfig = {
 
   var database = firebase.database();
 
-  function get(){
-    var user_ref = database.ref('users/' + 'League of Legends')
-    user_ref.on('value', function(snapshot){
-        var data = snapshot.val()
-        console.log(data.username)
-    })
+  function displayUserData() {
+    // Retrieve user data from Firebase
+    var usersRef = database.ref('users/' + 'League of Legends');
+    usersRef.once('value').then(function(snapshot) {
+      var userData = snapshot.val();
+  
+      // Update user container 1
+      var userContainer1 = document.getElementById('userContainer1');
+      userContainer1.querySelector('.username').textContent = userData.user1.username;
+      userContainer1.querySelector('.rank').textContent = userData.user1.rank;
+      userContainer1.querySelector('.role').textContent = userData.user1.role;
+  
+      // Update user container 2
+      var userContainer2 = document.getElementById('userContainer2');
+      userContainer2.querySelector('.username').textContent = userData.user2.username;
+      userContainer2.querySelector('.rank').textContent = userData.user2.rank;
+      userContainer2.querySelector('.role').textContent = userData.user2.role;
+  
+      // Update user container 3
+      var userContainer3 = document.getElementById('userContainer3');
+      userContainer3.querySelector('.username').textContent = userData.user3.username;
+      userContainer3.querySelector('.rank').textContent = userData.user3.rank;
+      userContainer3.querySelector('.role').textContent = userData.user3.role;
+    });
   }
+
+// Call the function to display user data
+displayUserData();
+
