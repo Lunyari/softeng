7410a94e29244dea0b2583c39041d4b41bad4d58
clarence <arenzmauricio12@gmail.com>
2023-07-07 09:02:43 +0800

    a

--- register.js
+++ register.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC57Ph0aBB7V07w1eeuojP5NN5KSF43HKw",
-  authDomain: "nexusparty.firebaseapp.com",
-  databaseURL: "https://nexusparty-default-rtdb.firebaseio.com",
-  projectId: "nexusparty",
-  storageBucket: "nexusparty.appspot.com",
-  messagingSenderId: "263956260738",
-  appId: "1:263956260738:web:0ad730fe7effc588e5919b",
-  measurementId: "G-F97497CMSL"
+  apiKey: "AIzaSyDRkn5GW4Y8GsupHIZcJa_DZ7TDyooAsM0",
+  authDomain: "nexusparr.firebaseapp.com",
+  projectId: "nexusparr",
+  storageBucket: "nexusparr.appspot.com",
+  messagingSenderId: "959208000955",
+  appId: "1:959208000955:web:71ba88ca146826901b489e",
+  measurementId: "G-TSJNXT3M9N"
 };
 
 
