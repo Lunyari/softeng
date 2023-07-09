f8f60ee6f8a4edbd551d68d1fef2b364ff2efa82
clarence <arenzmauricio12@gmail.com>
2023-07-10 01:38:27 +0800

    about

---
+++ php/about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/css/about.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <link href="./about.css" rel="stylesheet" />
+        <div class="about-container">
+          <div class="about-container1">
+            <img alt="image" src="/nexus_p_arty-1500w.jpg" class="about-image" />
+          </div>
+          <h1 class="about-text">What is Nexus Party?</h1>
+          <span class="about-text01">
+            <span>
+                      Nexus Party is a user-friendly web application that helps gamers
+              find a good teammate or a player who is matched with their skills. This
+              application will help people have an easier time finding players to
+              match with for a better gaming experience, in the long term, this will
+              decrease the amount of people having a hard time enjoying the game solo
+              and give a new experience to solo players what it&apos;s like to play as
+              a premade team.
+            </span>
+            <br />
+            <br />
+            <span>       </span>
+            <span>
+              Some objectives of Nexus party are to be able to help players easily
+              find their desired teammate to play with. To create a more sociable
+              environment for all ages of people who play the same games. To allow
+              players to communicate with each other using a chat feature. To let
+              players find their desired teammates based on the profile that shows
+              their current rankings created by other players. To allow players to
+              show their skill levels by putting in their ranks in the game they play
+              with the profile customization feature. To provide a new experience to
+              players who are used to playing solo by playing and communicating with
+              different players.
+            </span>
+            <br />
+            <br />
+          </span>
+          <div class="about-container2">
+            <span class="about-text09">
+              <span>Get In Touch </span>
+              <br />
+              <span>       Now</span>
+              <br />
+            </span>
+            <button type="button" class="about-button button">
+              <span>
+                <span>Contact us</span>
+                <br />
+              </span>
+            </button>
+            <span class="about-text17">Nexus Party @ facebook</span>
+          </div>
+          <button
+            id="homeBtn"
+            name="homeBtn"
+            type="button"
+            class="about-button1 button"
+          >
+            <span>
+              <span>Home</span>
+              <br />
+            </span>
+          </button>
+          <div class="about-container3"><h1 class="about-text21">CREATORS</h1></div>
+          <div class="about-container4">
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8c2VhcmNofDF8fHByb2ZpbGV8ZW58MHx8fHwxNjg4ODUyNzc5fDA&amp;ixlib=rb-4.0.3&amp;q=80&amp;h=800"
+              class="about-image1"
+            />
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8c2VhcmNofDF8fHByb2ZpbGV8ZW58MHx8fHwxNjg4ODUyNzc5fDA&amp;ixlib=rb-4.0.3&amp;q=80&amp;h=800"
+              class="about-image2"
+            />
+            <img
+              alt="image"
+              src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8c2VhcmNofDF8fHByb2ZpbGV8ZW58MHx8fHwxNjg4ODUyNzc5fDA&amp;ixlib=rb-4.0.3&amp;q=80&amp;h=800"
+              class="about-image3"
+            />
+          </div>
+        </div>
+      </div>
+      
+    
+</body>
+</html>
